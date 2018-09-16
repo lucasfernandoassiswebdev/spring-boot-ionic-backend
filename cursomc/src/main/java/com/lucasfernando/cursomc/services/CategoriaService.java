57b20047d60c1a931acb6390de3477cb06d02009
lucasaassis@outlook.com <lucasaassis@outlook.com>
2018-09-16 18:17:51 -0300

    Adicionando endpoint de paginação

--- cursomc/src/main/java/com/lucasfernando/cursomc/services/CategoriaService.java
+++ cursomc/src/main/java/com/lucasfernando/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.lucasfernando.cursomc.domain.Categoria;
@@ -25,6 +28,11 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll() {		
 		return repository.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);			
+		return repository.findAll(pageRequest);		
 	}
 
 	public Categoria insert(Categoria obj) {
