7b72ea83beba5ce29d4d916caee5580ae2ebd008
lucasaassis@outlook.com <lucasaassis@outlook.com>
2018-10-03 22:48:03 -0300

    configurando postgres para o heroku

--- cursomc/src/main/java/com/lucasfernando/cursomc/config/TestConfig.java
+++ cursomc/src/main/java/com/lucasfernando/cursomc/config/TestConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Profile;
 import com.lucasfernando.cursomc.services.DBService;
 
 @Configuration
-@Profile("devpostgres") // configurações específicas do profile de teste(banco de dados em memódia)
+@Profile("devpostgres") 
 public class TestConfig {	
 	@Autowired
 	private DBService dbService;
